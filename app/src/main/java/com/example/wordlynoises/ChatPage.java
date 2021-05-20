530dffd2ebf967447b8bfc382c827b2dc46df5bf
Nareen Aydogan <nareenaydogan@gmail.com>
2021-05-20 00:57:58 -0500

    rough draftchat page

--- app/src/main/java/com/example/wordlynoises/ChatPage.java
+++ app/src/main/java/com/example/wordlynoises/ChatPage.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.WindowManager;
 
 public class ChatPage extends AppCompatActivity {
 
@@ -11,6 +12,7 @@ public class ChatPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat_page);
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN);
 
         String senderName = getIntent().getStringExtra("SENDER_NAME");
         //title
